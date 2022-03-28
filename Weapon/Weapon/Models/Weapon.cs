86fa610815309cab97b6722f8311c93723f0af62
IlhamEliyev <100667441+IlhamEliyev@users.noreply.github.com>
2022-03-28 13:47:58 +0400

    Weapon task

---
+++ Weapon/Weapon/Models/Weapon.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weapon.Models
+{
+    class WeaponC
+    {
+        private int _bulletCapacity;
+        private int _bulletCount;
+        private double _bulletDischargSec;
+        public int BulletCapacity { 
+            get 
+            {
+                return _bulletCapacity;
+            } 
+            set 
+            {
+                if (value>0 && value <= 200)
+                {
+                    _bulletCapacity = value;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input");
+                }
+            }
+        }
+        public int BulletCount { 
+            get 
+            {
+                return _bulletCount;
+            } 
+            set 
+            {
+                if (value >= 0 && value <= _bulletCapacity)
+                {
+                    _bulletCount = value;
+                }
+            } 
+        }
+        public double BulletDischargSec {
+            get 
+            {
+                return _bulletDischargSec;
+            }
+            set 
+            {
+                if (value > 0)
+                {
+                    _bulletDischargSec = value;
+                }
+            }
+        }
+        public WeaponC(int capacity, int count, double discharg)
+        {
+            BulletCapacity = capacity;
+            BulletCount = count;
+            BulletDischargSec = discharg;
+        }
+        public void Shoot() 
+        {
+            if (BulletCount == 0)
+            {
+                Console.WriteLine("Please use Reload method");
+            }
+            else
+            {
+                Console.WriteLine("Shooted 1 time");
+            }
+            BulletCount--;
+        }
+        public void Fire() 
+        {
+            double n = BulletCount / (BulletCapacity / BulletDischargSec);
+            BulletCount = 0;
+            Console.WriteLine(n);
+        }
+        public int GetBulletInfo() 
+        {
+            return BulletCount;
+        }
+        public void Reload() 
+        {
+            if (BulletCount >= 0 && BulletCount < BulletCapacity)
+            {
+                BulletCount = BulletCapacity;
+                Console.WriteLine("Reloaded");
+            }
+            else
+            {
+                Console.WriteLine("Ammo is full");
+            }
+        }
+    }
+}
---
+++ Weapon/Weapon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Weapon.Models;
+
+namespace Weapon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeaponC weapon = new WeaponC(22,10,5);
+            int input = 0;
+            while (input < 5 && input >= 0)
+            {
+                input = int.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        weapon.Fire();
+                        break;
+                    case 3:
+                        weapon.Reload();
+                        break;
+                    case 4:
+                        Console.WriteLine(weapon.GetBulletInfo());
+                        break;
+                    case 0:
+                        Console.WriteLine("1- Shoot, 2- Fire, 3- Reload, 4-GetBulletInfo");
+                        break;
+                }
+            }
+        }
+    }
+}
