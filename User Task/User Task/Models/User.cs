294b63aeee5cbe348a70f3bbcbf7ed194b31fc17
IlhamEliyev <100667441+IlhamEliyev@users.noreply.github.com>
2022-03-29 13:13:42 +0400

    User Task 3/29/2022

---
+++ User Task/User Task/Models/User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace User_Task.Models
+{
+    class User
+    {
+        private string _username;
+        private string _password;
+        public string Username { 
+            get 
+            {
+                return _username;
+            }
+            set
+            {
+                Regex rgx = new Regex(@"^[\w\.]{6,25}$");
+                if (rgx.IsMatch(value))
+                {
+                    _username = value.ToLower();
+                }
+            }
+        }
+        public string Password {
+            get 
+            {
+                return _password;
+            }
+            set
+            { 
+                bool upper = false;
+                bool lower = false;
+                bool digit = false;
+                foreach (char letter in value)
+                {
+                    if (!upper)
+                    {
+                        if (Char.IsUpper(letter))
+                        {
+                            upper = true;
+                        }
+                    }
+                    else if (!lower)
+                    {
+                        if (Char.IsLower(letter))
+                        {
+                            lower = true;
+                        }
+                    }
+                    else if(!digit)
+                    {
+                        if (Char.IsDigit(letter))
+                        {
+                            digit = true;
+                        }
+                    }
+                }
+                if (upper == true && lower == true && digit == true)
+                {
+                    _password = value;
+                }
+            }
+        }
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
---
+++ User Task/User Task/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using User_Task.Models;
+
+namespace User_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Username:
+            Console.WriteLine("Enter the username :");
+            string username = Console.ReadLine();
+            Regex rgx = new Regex(@"^[\w\.]{6,25}$");
+            if (!rgx.IsMatch(username))
+            {
+                Console.WriteLine("Wrong input! Please enter username again");
+                goto Username;
+            }
+            Password:
+            Console.WriteLine("Enter the password : ");
+            string password = Console.ReadLine();
+            bool upper = false;
+            bool lower = false;
+            bool digit = false;
+            foreach (char letter in password)
+            {
+                if (!upper)
+                {
+                    if (Char.IsUpper(letter))
+                    {
+                        upper = true;
+                    }
+                }
+                else if (!lower)
+                {
+                    if (Char.IsLower(letter))
+                    {
+                        lower = true;
+                    }
+                }
+                else if (!digit)
+                {
+                    if (Char.IsDigit(letter))
+                    {
+                        digit = true;
+                    }
+                }
+            }
+            if (!upper)
+            {
+                Console.WriteLine("Password must contain at least 1 uppercase");
+                goto Password;
+            }
+            else if (!lower)
+            {
+                Console.WriteLine("Password must contain at least 1 lowercase");
+                goto Password;
+            }
+            else if (!digit)
+            {
+                Console.WriteLine("Password must contain at least 1 digit");
+                goto Password;
+            }
+
+            User user = new User(username, password);
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("User.Username = " + user.Username);
+            Console.WriteLine("User.Password = " + user.Password);
+        }
+    }
+}
