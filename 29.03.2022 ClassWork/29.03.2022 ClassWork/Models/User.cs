da994d862ef3c18ac67e6c63afae72d6a787fcae
IlhamEliyev <100667441+IlhamEliyev@users.noreply.github.com>
2022-04-01 11:54:33 +0400

    salam

---
+++ 29.03.2022 ClassWork/29.03.2022 ClassWork/Models/User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _29._03._2022_ClassWork.Models
+{
+    class User
+    {
+        private string username;
+        private string password;
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                Regex regex = new Regex(@"^[\w\.]{6,25}$");
+                if (regex.IsMatch(value))
+                {
+                    username = value.ToLower();
+                }
+
+            }
+
+        }
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (value.Length > 8 && value.Length < 25)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        if (Char.IsUpper(value[i]))
+                        {
+
+                        }
+                        else if (Char.IsLower(value[i]))
+                        {
+
+                        }
+                        else if (!Char.IsDigit(value[i]))
+                        {
+
+                        }
+                        else
+                        {
+                            password = value;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 29.03.2022 ClassWork/29.03.2022 ClassWork/Program.cs
@@ -0,0 +1,26 @@
+﻿using _29._03._2022_ClassWork.Models;
+using System;
+using System.Text.RegularExpressions;
+
+namespace _29._03._2022_ClassWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the username : ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Please enter the password : ");
+            string password = Console.ReadLine();
+            Regex regex = new Regex(@"^[\w\.]{6,25}$");
+            if (!(regex.IsMatch(username)))
+            {
+                Console.WriteLine("Wrong input");
+            }
+            User user = new User(username, password);
+            Console.WriteLine(user.Username);
+            Console.WriteLine(user.Password);
+            
+        }
+    }
+}
